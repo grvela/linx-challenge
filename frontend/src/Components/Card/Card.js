8a6d67a695d2b47e47cee51eb8cf25921badf8c0
grvela <rodrigoandre777@gmail.com>
2020-09-13 22:45:18 -0400

    fixed new tab bug

--- frontend/src/Components/Card/Card.js
+++ frontend/src/Components/Card/Card.js
@@ -8,7 +8,7 @@ function Card({object, ...onclick}){
     return (
         <div className="container-centered link-item">
             <div id="url-item" className="title-Slab font-red" >
-                <a {...onclick} href={object.Link} rel="noopener noreferrer" target="_blank">{object.ShortLink}</a>
+                <a {...onclick} href={"https://" + object.Link} rel="noopener noreferrer" target="_blank">{object.ShortLink}</a>
                 </div>
             <div id="likes-item" className="title-Slab">{object.Hits}</div>
         </div>
